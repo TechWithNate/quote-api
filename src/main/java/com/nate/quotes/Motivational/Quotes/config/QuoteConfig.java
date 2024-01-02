edc3b07d4e727b4277cfbea4a70d06cd924808a3
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-02 02:47:04 +0000

    Fixing some minor errors

--- src/main/java/com/nate/quotes/Motivational/Quotes/config/QuoteConfig.java
+++ src/main/java/com/nate/quotes/Motivational/Quotes/config/QuoteConfig.java
@@ -15,11 +15,11 @@ public class QuoteConfig {
     @Bean
     CommandLineRunner commandLineRunner(QuoteRepository quoteRepository){
         return args -> {
-            Quote q1 = new Quote("This is quote 1", "Dzreke Nathan", QuoteCategory.AMBITION);
-            Quote q2 = new Quote("This is quote 2", "John Doe", QuoteCategory.ADVENTURE);
-            Quote q3 = new Quote("This is quote 3", "John Smith", QuoteCategory.CHARACTER);
-            Quote q4 = new Quote("This is quote 4", "Mike Baiths", QuoteCategory.CHANGE);
-            Quote q5 = new Quote("This is quote 5", "Jim Reeves", QuoteCategory.DREAMS);
+            Quote q1 = new Quote("Dzreke Nathan", "This is quote 1", QuoteCategory.AMBITION);
+            Quote q2 = new Quote("John Doe", "This is quote 2", QuoteCategory.ADVENTURE);
+            Quote q3 = new Quote("John Smith", "This is quote 3", QuoteCategory.CHARACTER);
+            Quote q4 = new Quote("Mike Baiths", "This is quote 4", QuoteCategory.CHANGE);
+            Quote q5 = new Quote("Jim Reeves", "This is quote 5", QuoteCategory.DREAMS);
             quoteRepository.saveAll(List.of(q1, q2, q3, q4, q5));
         };
     }
